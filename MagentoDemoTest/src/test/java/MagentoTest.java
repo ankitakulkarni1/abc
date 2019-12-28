d337c00619d62e1bec0daa998f1faba7f8d8245d
KULKARNI <KULKARNI@192.168.1.164>
2019-12-28 15:49:35 +0530

    commit 2

--- MagentoDemoTest/src/test/java/MagentoTest.java
+++ MagentoDemoTest/src/test/java/MagentoTest.java
@@ -10,7 +10,7 @@ public class MagentoTest {
 	@Test
 	public void MagTest(){
 		ChromeDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
 		Welcome w = new Welcome(driver);

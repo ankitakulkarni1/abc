41044021e28347e1d225c604afd32899432fe7c2
KULKARNI <KULKARNI@192.168.1.164>
2019-12-28 15:27:27 +0530

    commit 1

---
+++ MagentoDemoTest/src/test/java/MagentoTest.java
@@ -0,0 +1,30 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest {
+
+	@Test
+	public void MagTest(){
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https://www.magento.com");
+		Welcome w = new Welcome(driver);
+		w.clickOnMyAcct();
+		Login l = new Login(driver);
+		l.typeEmail("ankita17.abc@gmail.com");
+		l.typePass("Welcome1234");
+		l.clickOnLogin();
+		Main m = new Main(driver);
+		m.clickOnLogout();
+		driver.quit();
+		
+		
+		
+	}
+
+}
